fe8c4cbfc53489a6a8f52e8cfee76e19b92cacc5
Abolaji <guppyzey@gmail.com>
2025-05-29 17:19:54 +0100

    Post present file

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './index.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        white: '#fefefe',
+        black: '#0a0a0a',
+      }
+    },
+  },
+  plugins: [],
+}
+
